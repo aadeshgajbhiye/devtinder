bf5c62eeb33d1b52d4cdcf21b0195f563d5c840e
aadesh-code <agajbhiye725@gmail.com>
2025-05-15 17:40:23 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hi from tinder !");
+app.get("/users/:userID", (req, res) => {
+  // /users/:userID - its a dynamic routes means though api route itself we can able to send the data
+  // const { city } = req.query; this is the query we have send through the request with key and value pair
+  //console.log(number);
+  console.log(req.params);
+  res.send({ name: "Adesh", password: 123 });
+});
+
+app.post("/users", (req, res) => {
+  res.send("data received successfully");
+});
+app.use("/", (req, res) => {
+  res.send("Base");
 });
 
 app.listen(3000, () => {
